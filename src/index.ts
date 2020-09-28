aad912437aa84276140c2218fb1542bae4f1da50
Fabio Rodrigues <fabiordrig@gmail.com>
2020-09-28 19:33:03 -0300

    Bump version

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,8 @@ export * from "./errors/bad-request-error";
 export * from "./errors/conflict-error";
 export * from "./errors/custom-error";
 export * from "./errors/database-conn-error";
-
 export * from "./middlewares/current-user";
 export * from "./middlewares/error-handler";
 export * from "./middlewares/require-auth";
 export * from "./middlewares/validate-request";
-
 export * from "./constants/http";
