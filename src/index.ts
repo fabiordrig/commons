7f5a3cea1b700907fa24291ae222cff80066d729
Fabio Rodrigues <fabiordrig@gmail.com>
2020-09-28 19:02:46 -0300

    Bump version

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-export * from "./errors";
-export * from "./middlewares";
-export * from "./constants";
+export * from "./errors/not-found-error";
+export * from "./errors/request-validation-error";
+export * from "./errors/unauthorized-error";
+export * from "./errors/unprocessable-entity-error";
+export * from "./errors/bad-request-error";
+export * from "./errors/conflict-error";
+export * from "./errors/custom-error";
+export * from "./errors/database-conn-error";
+
+export * from "./middlewares/current-user";
+export * from "./middlewares/error-handler";
+export * from "./middlewares/require-auth";
+export * from "./middlewares/validate-request";
+
+export * from "./constants/http";
