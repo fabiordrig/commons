806aa3ded578ad98d545ac1951ec183d58876497
Fabio Rodrigues <fabiordrig@gmail.com>
2020-10-29 19:38:04 -0300

    Bump version

---
+++ src/@types/order.ts
@@ -0,0 +1,25 @@
+import { OrderStatus, Subjects } from "../constants";
+
+export interface OrderCancelledEvent {
+  subject: Subjects.OrderCancelled;
+  data: {
+    id: string;
+    ticket: {
+      id: string;
+    };
+  };
+}
+
+export interface OrderCreatedEvent {
+  subject: Subjects.OrderCreated;
+  data: {
+    id: string;
+    status: OrderStatus;
+    userId: string;
+    expiresAt: string;
+    ticket: {
+      id: string;
+      price: number;
+    };
+  };
+}
